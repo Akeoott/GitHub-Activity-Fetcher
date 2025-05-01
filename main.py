ae195b91b49e9c6058f90542c67d68bfbc99cd2d
Akeoott <amejanin00@gmail.com>
2025-05-01 21:18:21 +0200

    Added comments and improved code readability

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import requests,sys
 import traceback
 
+
 try:
     while True:
+
+        # User input handling
         while True:
             try:
                 print("Select what you want to fetch:\nList repository events (enter 1)\nList events for the authenticated user (enter 2)\n")
-
+                
+                # Asking what one specifically wants to fetch
                 selected_fetch = int(input("Enter here: "))
 
                 possible_selection = {1,2}
@@ -29,23 +33,30 @@ try:
         url_events = f'https://api.github.com/users/{username}/events'
         url_repo_events = f'https://api.github.com/repos/{username}/{repo}/events'
 
+        # Asking for a token
+        token_github_docs_info = "https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens"
+
         while True:
             if selected_fetch == 1:
                 print("You require a personal access token.")
                 print("Dont know how to get a token? ", end='')
-                print("\033]8;;https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens\033\\Visit this website.\033]8;;\033\\")
+                print(f"\033]8;;{token_github_docs_info}\033\\Visit this website.\033]8;;\033\\")
+
                 token = input('\nEnter your access token (case sensitive): ')
                 break
+
             elif selected_fetch == 2:
                 print("You may want to use a personal access token.")
                 print("Dont know how to get a token? ", end='')
-                print("\033]8;;https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens\033\\Visit this website.\033]8;;\033\\")
+                print(f"\033]8;;{token_github_docs_info}\033\\Visit this website.\033]8;;\033\\")
+
                 token = input('\nOptional -> Enter your access token. If not, enter "no" (case sensitive): ')
                 if token == "no":
                     token = None
                 break
         break
-
+    
+    # Confirming input is correct
     print(f"\nIs this correct?\nUsername: {username}\nApp name or identifier: {UserAgent}\nRepository: {repo}\nYour token: {token}\n")
     if token is None:
         print("(Your token is None, reason is you either have entered it wrong or you didnt want to enter one)\n")
@@ -66,16 +77,19 @@ try:
     if token:
         headers['Authorization'] = f'token {token}'
 
+    # Selecting different ways to fetch specific data
     if selected_fetch == 1:
         response = requests.get(url_repo_events, headers=headers)
     elif selected_fetch == 2:
         response = requests.get(url_events, headers=headers)
 
+    # Fetchng and displaying User Events
     if response.status_code == 200:
         data = response.json()
 
         print("\nUser Events:", data)
 
+        # Fetching rate limits
         rate_limit = response.headers.get('X-RateLimit-Limit')
         rate_remaining = response.headers.get('X-RateLimit-Remaining')
         rate_reset = response.headers.get('X-RateLimit-Reset')
@@ -91,10 +105,11 @@ try:
         print(f"Error: {response.status_code}")
         print("More informations under: https://docs.github.com/rest/using-the-rest-api/troubleshooting-the-rest-api?apiVersion=2022-11-28")
 
+# Error handeling for unexpected errors as i do silly mistakes
 except Exception as e:
     print("\nAn unexpected error occurred:")
     print(f"{type(e).__name__}: {e}")
-    print("Please report this issue with the above error message to GitHub 'Akeoots/GitHub-Activity-Fetcher'.")
+    print("\nPlease report this issue with the above error message on GitHub 'Akeoots/GitHub-Activity-Fetcher/issues'.")
 
 input("\nPress Enter To Exit...")
 sys.exit()
